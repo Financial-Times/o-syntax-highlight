678b5a304c5c7947a4daf6af9ffe11d257123b0b
Gabrielle von Koss <gabivk@gmail.com>
2018-08-15 15:31:05 +0100

    smol demo fix

--- src/js/syntax-highlight.js
+++ src/js/syntax-highlight.js
@@ -4,8 +4,7 @@ import prism from 'prism/prism.js';
 import diff from './languages/prism-diff.js';
 import loadLanguages from 'prism/components/index.js';
 // Adds to Prism global object which we remove https://github.com/PrismJS/prism/blob/v1.15.0/prism.js#L6
-require('prism/components/prism-bash.js')
-// require('prism/components/prism-css.js');
+require('prism/components/prism-bash.js');
 require('prism/components/prism-json.js');
 require('prism/components/prism-scss.js');
 
